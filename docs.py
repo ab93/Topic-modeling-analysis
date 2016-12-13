8470ca42151dfc20b2240e2e6ebc77dfb9258a2c
Avik Basu <avikbasu93@gmail.com>
2016-12-13 13:45:18 -0800

    Added multi-topics per doc method

--- docs.py
+++ docs.py
@@ -5,7 +5,13 @@ import pandas as pd
 from collections import defaultdict, OrderedDict, Counter
 import matplotlib.pyplot as plt
 
-def doc_topics(gamma_file, paper_id_file, output_file):
+def convert_gamma_to_prob(gamma_file, output_path=None):
+    df = pd.read_csv(gamma_file, delim_whitespace=True, header=None)
+    df = df.apply(lambda x: x/np.sum(x), axis=1, raw=True)
+    probs = df.values
+    return np.argsort(-probs, axis=1)
+
+def find_doc_topics_single(gamma_file, paper_id_file, output_file):
     output_dir = '/'.join(output_file.split('/')[:-1])
     gamma_df = pd.read_csv(gamma_file, delim_whitespace=True, header=None)
     num_docs = len(gamma_df)
@@ -19,7 +25,7 @@ def doc_topics(gamma_file, paper_id_file, output_file):
     
     with open(output_file, 'w') as outfile:
         outfile.write('TOPIC ID' + '\t' + 'PAPER_ID' + '\n')
-        for topic in topic_doc_map:
+        for topic in topic_doc_map: 
             print topic
             for paper in topic_doc_map[topic]:
                 outfile.write(str(topic) + '\t' + str(paper) + '\n')
@@ -27,15 +33,40 @@ def doc_topics(gamma_file, paper_id_file, output_file):
     plt.xlabel('Topic ID')
     plt.ylabel('Number of documents')
     plt.bar(range(len(topic_count)), topic_count.values())
-    plt.xticks(range(0,len(topic_count),5))
+    plt.xticks(range(0,len(topic_count),8))
     plt.savefig(output_dir + '/distribution.png')
 
+
+def find_doc_topics(gamma_file, paper_id_file, output_file, n=3):
+    n = int(n)
+    output_dir = '/'.join(output_file.split('/')[:-1])
+    doc_topics = convert_gamma_to_prob(gamma_file)[:,:n]
+
+    num_docs = doc_topics.shape[0]
+    paper_ids = np.loadtxt(paper_id_file, dtype=int)
+
+    topic_doc_map = defaultdict(list)
+
+    for i in xrange(num_docs):
+        for j in range(n):
+            topic_doc_map[doc_topics[i,j]].append(paper_ids[i])
+    
+    with open(output_file, 'w') as outfile:
+        outfile.write('TOPIC ID' + '\t' + 'PAPER_ID' + '\n')
+        for topic in topic_doc_map: 
+            print topic
+            for paper in topic_doc_map[topic]:
+                outfile.write(str(topic) + '\t' + str(paper) + '\n')
+
+
 if (__name__ == '__main__'):
-    if (len(sys.argv) != 4):
-        print 'usage: python topics.py <gamma-file> <paper-id-file> <output_file>\n'
+    if len(sys.argv) == 5:
+        gamma_file = sys.argv[1]
+        paper_id_file = sys.argv[2]
+        output_file = sys.argv[3]
+        n = sys.argv[4]
+        find_doc_topics(gamma_file, paper_id_file, output_file, n)
+    
+    else:
+        print 'usage: python docs.py <gamma-file> <paper-id-file> <output_file> <top_n>'
         sys.exit(1)
-            
-    gamma_file = sys.argv[1]
-    paper_id_file = sys.argv[2]
-    output_file = sys.argv[3]
-    doc_topics(gamma_file, paper_id_file, output_file)
